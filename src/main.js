57854972d25684004f2f2ba7431980008be824a6
Siddhesh Mangela <sidthecool007@gmail.com>
2019-04-05 12:21:56 +0530

    semicolon bug fixed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import LongPress from 'vue-directive-long-press'
+import LongPress from 'vue-directive-long-press';
 import App from './App.vue';
 import router from './router';
 import store from './store';
